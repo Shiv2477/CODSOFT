730fe9147dd4ba044df8684f2f65941c6a851458
Shiv2477 <142168982+Shiv2477@users.noreply.github.com>
2024-07-13 13:00:41 +0530

    calculator.py

--- calculator.py
+++ calculator.py
@@ -11,11 +11,12 @@ def calculate(n1,n2,op):
 		result=n1^n2
 	return result
 number1=float(input('Enter the first number:'))
-op=input('Enter the operator(+,-,*,/,^)')
-number1=float(input('Enter the second number:'))
+op=input('Enter the operator(+,-,*,/,^)   ')
+number2=float(input('Enter the second number:'))
 print(number1,op,number2)
 result=calculate(number1,number2,op)
 print('=',result)
+
 
 
 
